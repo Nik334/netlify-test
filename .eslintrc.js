d9cae51b45835aeb9423c4db997dcf2e0dc15d67
nikhil1 <nikkumar334@gmail.com>
2024-07-23 23:57:30 +0530

    added few files

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// .eslintrc.js
+module.exports = {
+    "rules": {
+        "no-unused-vars": "warn", // Change 'error' to 'warn' for unused variables
+        "react-hooks/exhaustive-deps": "warn" // Change 'error' to 'warn' for missing dependencies in hooks
+    }
+};
