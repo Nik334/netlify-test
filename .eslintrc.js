010f1a524b04b3414cd5ade388fdbac91920c3a2
nikhil1 <nikkumar334@gmail.com>
2024-07-24 00:04:48 +0530

    added dependencie eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 // .eslintrc.js
 module.exports = {
+    "plugins": [
+    "react-hooks"
+],
     "rules": {
-        "no-unused-vars": "warn", // Change 'error' to 'warn' for unused variables
-        "react-hooks/exhaustive-deps": "warn" // Change 'error' to 'warn' for missing dependencies in hooks
-    }
+    "react-hooks/exhaustive-deps": "warn"
+}
 };
